599dd65bff889a2bfe57231aa9059498666d76a5
Alyssa Liguori <alyssa.liguori@gmail.com>
2018-12-05 22:11:30 -0500

    csrf and token

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const csrf= require('csurf')
+const csrfProtection = csrf();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('shop/index', { title: 'girl' });
 });
 
+router.get('/user/signup', function(req, res, next){
+  res.render('user/signup', {csrfToken: req.csrfToken()});
+});
 module.exports = router;
